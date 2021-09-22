e7ac1bce17b64ac6ed5a6f459ae315319f526adc
Bryant <bratik121@gmail.com>
2021-09-22 19:33:16 -0400

    se añadio hasta eventos

--- codigo.js
+++ codigo.js
@@ -26,3 +26,31 @@ titulo.classList.add("titulo");
 contenedor.appendChild(titulo);
 
 //!capitulo 9 eventos
+
+let boton = document.querySelector(".boton");
+let contenedor2 = document.querySelector(".contenedor-2");
+/*
+boton.onclick = () => {
+	let span = document.createElement("SPAN"); 
+	span.innerHTML = "You are not alone</br>";
+	contenedor2.appendChild(span);
+	*No se suele usar 
+};*/
+/*
+boton.addEventListener("click", añadirTexto);
+
+function añadirTexto() {
+	let span = document.createElement("SPAN");
+	span.innerHTML = "Correcto</br>";
+	contenedor2.appendChild(span);
+	//boton.removeEventListener("click", añadirTexto);//*remueve el evento
+	//*Manera correcta de hacerlo
+}*/
+
+//?otra manera de hacerlo seria
+
+boton.addEventListener("click", (e) => {
+	let span = document.createElement("SPAN");
+	span.innerHTML = "Correcto</br>";
+	contenedor2.appendChild(span);
+});
